0a891e806a9114f85fbd546b220fd157a7e59129
ris-h-av <risabk444@gmail.com>
2024-10-23 19:27:49 +0530

    document editor page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   description: "documnet collaborator",
 };
 
-export default function RootLayout({children,}: {children: React.ReactNode;}) {
+export default function RootLayout({children}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
       <body
